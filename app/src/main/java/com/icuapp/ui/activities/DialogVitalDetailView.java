7e67a2b0ac2386271ee577b16b165ee4bbfd2348
Hardik Jain <hardik.jain@scorgtechnologies.com>
2017-07-05 20:22:17 +0530

    update UI by Hardik

--- app/src/main/java/com/icuapp/ui/activities/DialogVitalDetailView.java
+++ app/src/main/java/com/icuapp/ui/activities/DialogVitalDetailView.java
@@ -11,16 +11,64 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.icuapp.R;
+import com.icuapp.customesViews.CustomTextView;
+import com.icuapp.model.vitals.VitalDetails;
+import com.icuapp.util.AppConstants;
+import com.icuapp.util.CommonMethods;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by jeetal on 4/7/17.
  */
 
 public class DialogVitalDetailView extends AppCompatActivity {
+
+//    @BindView(R.id.vitalsMainTagCount)
+//    CustomTextView mVitalsMainTagCount;
+//
+//    @BindView(R.id.bedNo)
+//    CustomTextView mBedNo;
+//
+//    @BindView(R.id.patientName)
+//    CustomTextView mPatienName;
+
+    @BindView(R.id.countPulse)
+    CustomTextView mPulseCount;
+
+    @BindView(R.id.countPleth)
+    CustomTextView mSpo2Count;
+
+    @BindView(R.id.countResp)
+    CustomTextView mRespCount;
+
+    @BindView(R.id.countCvp)
+    CustomTextView mCvpCount;
+
+    @BindView(R.id.countIcp)
+    CustomTextView mIcpCount;
+
+    @BindView(R.id.countPap)
+    CustomTextView mPapCount;
+
+    @BindView(R.id.countSystolicPressure)
+    CustomTextView mSysPressureCount;
+
+    @BindView(R.id.countt1)
+    CustomTextView mT1Count;
+
+    @BindView(R.id.countt2)
+    CustomTextView mT2Count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_vital_detail_view_layout);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
@@ -35,7 +83,35 @@ public class DialogVitalDetailView extends AppCompatActivity {
                 finish();
             }
         });
+        setupPatientVitals();
 
+    }
 
+    private void setupPatientVitals() {
+        ArrayList<VitalDetails> vitalInfo = AppConstants.getVitalInfo(CommonMethods.generateRandomEvenNumber());
+        for (VitalDetails dataObject :
+                vitalInfo) {
+            String name = dataObject.getName();
+            String value = dataObject.getValue();
+            if (name.equalsIgnoreCase("Pleth") || name.equalsIgnoreCase("SPO2")) {
+                mSpo2Count.setText(value);
+            } else if (name.equalsIgnoreCase("Resp")) {
+                mRespCount.setText(value);
+            } else if (name.equalsIgnoreCase("CVP")) {
+                mCvpCount.setText(value);
+            } else if (name.equalsIgnoreCase("ICP")) {
+                mIcpCount.setText(value);
+            } else if (name.equalsIgnoreCase("PAP")) {
+                mPapCount.setText(value);
+            } else if (name.equalsIgnoreCase("Pulse")) {
+                mPulseCount.setText(value);
+            } else if (name.equalsIgnoreCase("Systolic Pressure")) {
+                mSysPressureCount.setText(value);
+            } else if (name.equalsIgnoreCase("T1")) {
+                mT1Count.setText(value);
+            } else if (name.equalsIgnoreCase("T2")) {
+                mT2Count.setText(value);
+            }
+        }
     }
 }
--- app/src/main/java/com/icuapp/ui/activities/ECGGraphDetail.java
+++ app/src/main/java/com/icuapp/ui/activities/ECGGraphDetail.java
@@ -3,6 +3,8 @@ package com.icuapp.ui.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.icuapp.R;
 
@@ -16,5 +18,28 @@ public class ECGGraphDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ecg_graph_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("ECG Graphs");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
