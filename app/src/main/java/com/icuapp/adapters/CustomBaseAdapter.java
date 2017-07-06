b1cf5db4ef05ab41af02b53b0933af3692631488
riteshPScorge <ritesh.pandhurkar@scorgtechnologies.com>
2017-07-06 11:04:54 +0530

    ui fixed

--- app/src/main/java/com/icuapp/adapters/CustomBaseAdapter.java
+++ app/src/main/java/com/icuapp/adapters/CustomBaseAdapter.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
